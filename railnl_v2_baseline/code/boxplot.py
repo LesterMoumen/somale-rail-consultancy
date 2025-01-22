254dc2f59b0aa54f4d8bcce0dab96ba921e04f93
LesterMoumen <l.y.moumen@gmail.com>
2025-01-22 13:33:10 +0100

    Small changes to main

--- railnl_v2_baseline/code/boxplot.py
+++ railnl_v2_baseline/code/boxplot.py
@@ -5,6 +5,7 @@ from code.classes.train_table import Train_table
 def create_box_plot(connections, locations, number_of_trajects, max_time, start_location_algorithm, select_next_station_algoritm, N=100, plot_name=None):
     #connections_file, locations_file, number_of_trajects, max_time, N=100, plot_name=None):
 
+    best_quality = 0
     data = []
     for trajects in range(1, number_of_trajects+1):
 
@@ -16,7 +17,12 @@ def create_box_plot(connections, locations, number_of_trajects, max_time, start_
             quality = int(train_table_object.calculate_quality())
             traject_data.append(quality)
 
+            if quality > best_quality:
+                best_quality = quality
+                best_train_table = train_table_object
+
         data.append(traject_data)
+
 
     # Make the plot
     plt.boxplot(data)
@@ -24,3 +30,5 @@ def create_box_plot(connections, locations, number_of_trajects, max_time, start_
     plt.ylabel("Quality score")
     plt.title(plot_name)
     plt.show()
+
+    return best_train_table, best_quality
--- railnl_v2_baseline/main.py
+++ railnl_v2_baseline/main.py
@@ -5,6 +5,9 @@ from code.algorithms.randomise import random_start_station
 from code.algorithms.randomise import random_select_next_station
 from code.classes.trajectanalyzer import *
 from code.boxplot import create_box_plot
+from code.algorithms.depthfirst import DepthFirstCounter
+from code.classes.station import Station
+
 
 # Data files
 locations_holland = "data/StationsHolland_locaties.csv"
@@ -15,32 +18,39 @@ connections_national = "data/ConnectiesNationaal.csv"
 # Parameters
 connections = connections_holland
 locations = locations_holland
-number_of_trajects = 4
+number_of_trajects = 7
 max_time = 120
 
 if __name__ == "__main__":
     # Create baseline model
     baseline_train_table = Train_table(connections, locations, number_of_trajects, max_time, start_location_algorithm = random_start_station, select_next_station_algoritm = random_select_next_station)
-    baseline_train_table.create_table()
+    baseline_train_table.loop_for_solution()
 
-    # Printing output in terminal
-    #baseline_train_table.print_output()
+    #Printing output in terminal
+    baseline_train_table.print_output()
 
-    # Creates csv output file and adds to output folder
-    #baseline_train_table.output_to_csv()
+    #Creates csv output file and adds to output folder
+    baseline_train_table.output_to_csv()
 
-    # Creates visualisatin plot
-    #baseline_train_table.visualisation()
+    #Creates visualisatin plot
+    baseline_train_table.visualisation()
 
     #stations_dict = baseline_train_table.stations_dict
     #ta = TrajectAnalyzer(stations_dict) #, traject_histories)
 
-    #______TrajectAnalyzer tester______
-    stations_dict = baseline_train_table.stations_dict
-    ta = TrajectAnalyzer(stations_dict) #, traject_histories)
-    print(f"\n(Remaining) Dead ends: \n{ta.dead_ends}")
-    print(f"\n(Remaining) Odd connections: \n{ta.odd_connections}")
-    print(f"\n(Next) Start location: {ta.next_start_location}")
+    # #______TrajectAnalyzer tester______
+    # stations_dict = baseline_train_table.stations_dict
+    # ta = TrajectAnalyzer(stations_dict) #, traject_histories)
+    # print(f"\n(Remaining) Dead ends: \n{ta.dead_ends}")
+    # print(f"\n(Remaining) Odd connections: \n{ta.odd_connections}")
+    # print(f"\n(Next) Start location: {ta.next_start_location}")
+    #
+    # #______TrajectAnalyzer tester______
+    # depth_first = DepthFirstCounter(stations_dict, max_time)
+    # # Count all possible trajectories for the map
+    # total_possible_trajectories = depth_first.count_all_possible_trajectories()
+    # print(f"Total possible trajectories for the map within {max_time} minutes: {total_possible_trajectories}")
+
 
     #______Box plot maker for random algorithm______
     #number_of_experiments = 100 # N
@@ -48,3 +58,8 @@ if __name__ == "__main__":
     #plot_name = f"K Distributie Random Algoritme (N = {number_of_experiments})"
 
     #create_box_plot(connections, locations, number_of_trajects, max_time, start_location_algorithm = random_start_station, select_next_station_algoritm = random_select_next_station, N = number_of_experiments, plot_name = plot_name)
+
+
+# -------------Greedy ----------------------------
+# greedy = gr.Greedy(baseline_train_table())
+# greedy.run()
