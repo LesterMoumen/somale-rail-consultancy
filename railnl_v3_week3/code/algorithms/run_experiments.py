23f51c9e7192572540c3f552fd11524ff3f619f0
Soesja van der Hilst <svdhilst@gmail.com.>
2025-01-29 17:32:49 +0100

    randomise now also works and saves a boxplot

--- railnl_v3_week3/code/algorithms/run_experiments.py
+++ railnl_v3_week3/code/algorithms/run_experiments.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
 from code.algorithms.experiment import Experiment
+from code.classes.visualisation import Visualisation
 from code.algorithms.randomise import Randomise
 from code.classes.helper_functions import save_results
 from code.algorithms.hillclimber import HillClimber
 from code.algorithms.simulatedannealing import SimulatedAnnealing
+from code.algorithms.greedy import Greedy
+from code.algorithms.greedy import GreedyLookahead
 
 
 class RunExperiments():
-    def __init__(self, connections_file, locations_file, max_number_of_trajects, max_time, number_of_experiments1, number_of_experiments2, algorithm1_type, algorithm2_type, use_randomise = False):
+    def __init__(self, connections_file, locations_file, max_number_of_trajects, max_time, number_of_experiments1, number_of_experiments2, algorithm1_type, algorithm2_type, start_trajects = 7, end_trajects = 17, use_randomise = False):
         self.connections_file = connections_file
         self.locations_file = locations_file
         self.max_number_of_trajects = max_number_of_trajects
@@ -17,17 +20,19 @@ class RunExperiments():
         self.algorithm1 = algorithm1_type
         self.algorithm2 = algorithm2_type
         self.use_randomise = use_randomise
-        self.experiment_object_dict = {}
+        self.start_trajects = start_trajects
+        self.end_trajects = end_trajects
+        self.experiment_object_dict = {} #stores the best experiment objects for each number of trajects
 
         # List of lists of qualities
         # e.g. [traject1_data, traject2_data] with traject1_data = [experiment1_quality, experiment2_quality]
-        self.data = {}
+        self.data = {} #stores all quality scores of traject count
 
     def run_first_algorithm(self):
         """ Runs all experiments and collects data. Saves best quality experiment too.
         """
 
-        for number_of_trajects in range(11, 12): #self.max_number_of_trajects+1):
+        for number_of_trajects in range(self.start_trajects, self.end_trajects + 1):
             highest_quality = 0
             qualities = []
             best_experiment_object = None
@@ -35,8 +40,13 @@ class RunExperiments():
             print(f"Starting experiments for {number_of_trajects} trajects...")
 
             for i in range(self.number_of_experiments1):
+                # checks if an object has a randomise atribute
+                if 'use_randomise' in self.algorithm1.__init__.__code__.co_varnames: #hasattr(self.algorithm1, '__init__') and
+                    experiment_object = self.algorithm1(self.connections_file, self.locations_file, number_of_trajects, self.max_time, use_randomise = self.use_randomise)
+                else:
+                    experiment_object = self.algorithm1(self.connections_file, self.locations_file, number_of_trajects, self.max_time)
+
                 # Initialize the experiment object
-                experiment_object = self.algorithm1(self.connections_file, self.locations_file, number_of_trajects, self.max_time)
                 experiment_object.run()
 
                 # Print progress every 100 iterations
@@ -59,12 +69,18 @@ class RunExperiments():
             self.data[number_of_trajects] = qualities
 
 
-    def save_all_objects(self, state_name):
+    def save_all_objects(self, state_name, algorithm):
+        """
+
+        """
         for traject_count, experiment_object in self.experiment_object_dict.items():
-            save_results(experiment_object, traject_count, state_name)
+            save_results(experiment_object, traject_count, state_name, algorithm)
 
 
     def run_second_algorithm(self, temperature):
+        """
+        Run second algorithm
+        """
         for traject_count, experiment_object in self.experiment_object_dict.items():
 
             # If using SimulatedAnnealing, pass the temperature
@@ -81,15 +97,52 @@ class RunExperiments():
             optimized_train_table = algorithm.train_table
             self.experiment_object_dict[traject_count] = optimized_train_table
 
-    # def visualise(self):
-    #     self.experiment_object_dict.visualisation()
-
-
-    def create_boxplot(self):
-        """ Display data as boxplot.
+    def box_plot(self, filename = None):
         """
-        plt.boxplot(self.data)
+        Create a box plot and if filename exist, png of boxplot will be saved in output folder
+        """
+
+        # check if self.data is empty
+        if not self.data:
+            print("no distribution availible")
+            return
+
+        # create boxplot of colected data
+        plt.boxplot(self.data.values(), labels = self.data.keys())
         plt.xlabel("Number of trajects")
         plt.ylabel("Quality score")
-        plt.title(f"Box plot of experiment data (N = {self.number_of_experiments*self.max_number_of_trajects})")
-        plt.show()
+        plt.title(f"Box plot of experiment data, {self.number_of_experiments1} iterations from {self.start_trajects} till {self.end_trajects} trajects ")
+
+        if filename:
+            plt.savefig(f"output/{filename}.png")
+            print(f"Boxplot saved as {filename}")
+
+        if filename == None:
+            plt.show()
+
+    # def visualise(self):
+    #     self.experiment_object_dict.visualisation()
+    # def visualise_experiment(self, filename = None):
+    #
+    #     # check if there are results in experiment_object_dict
+    #     if not self.experiment_object_dict:
+    #         print("there is no best experiment so no visualisation possible")
+    #         return
+    #
+    #     for number_of_trajects, experiment_object in self.experiment_object_dict.items():
+    #         visualisation = Visualisation(experiment_object.stations_dict, experiment_object.connections_dict, experiment_object.traject_list)
+    #         if filename:
+    #             visualization_filename = f"output/{filename}_visualization.png"
+    #             visualisation.save_visualisation(visualization_filename)
+    #         if filename == None:
+    #             visualisation.show_visualisation()
+    # #
+
+    # def create_boxplot(self):
+    #     """ Display data as boxplot.
+    #     """
+    #     plt.boxplot(self.data)
+    #     plt.xlabel("Number of trajects")
+    #     plt.ylabel("Quality score")
+    #     plt.title(f"Box plot of experiment data (N = {self.number_of_experiments*self.max_number_of_trajects})")
+    #     plt.show()
