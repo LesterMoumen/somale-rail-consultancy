4ada9dd8dbe438f453cd8351d2f790bf14dc0070
Soesja van der Hilst <svdhilst@gmail.com.>
2025-01-29 22:29:36 +0100

    greedy en greedylookahead scripts toegevoegd

--- railnl_v3_week3/main.py
+++ railnl_v3_week3/main.py
@@ -24,6 +24,8 @@ connections_file = connections_national
 locations_file = locations_national
 max_number_of_trajects = 10
 max_time = 180
+start_trajects = 5 # The minimum you need to visit all the connections in the Netherlands
+end_trajects = 20 # The maximum of trajects if you use the Netherlands
 
 if __name__ == "__main__":
     # ____Run single experiment____
@@ -79,33 +81,82 @@ if __name__ == "__main__":
     # greedy_experiment.visualisation("greedy visualisation")
 
     #  __________GreedyLookahead___________
-    greedy_lookahead_experiment = GreedyLookahead(connections_file, locations_file, max_number_of_trajects, max_time, use_randomise = False)
+    # greedy_lookahead_experiment = GreedyLookahead(connections_file, locations_file, max_number_of_trajects, max_time, lookahead_depth= 2, use_randomise = False)
     # greedy_lookahead_experiment.run()
     # greedy_lookahead_experiment.print_output()
 
+    # ____________Greedy________________________:
+    # Note: The experiment for the greedy algoritm needs to run 1 time because it uses the heuristics of the
+    # TrajectAnalyzer so every route will be the same.
 
-    #__________GreedyLookahead random experiment___________
-    algorithm1 = GreedyLookahead
-    algorithm2 = None
-    number_of_experiments1 = 2
-    r = RunExperiments(connections_file, locations_file, max_number_of_trajects, max_time,
-                        number_of_experiments1, number_of_experiments2 = None, algorithm1_type = algorithm1, algorithm2_type = algorithm2, use_randomise = True)
-    r.run_first_algorithm()
-    r.save_all_collected_data("Greed")
-    # r.save_all_objects("GreedyLookahead random", algorithm1)
-    # r.print()
-    # r.visualise_experiment("greedy_look")
-    # r.to_csv()
-    # r.box_plot("GreedyLookahead random")
-
-    # __________ GreedyLookahead TrajectAnalyzer experiment_________
-    algorithm1 = GreedyLookahead
-    algorithm2 = None
-    number_of_experiments1 = 2
-    start_trajects = 7
-    end_trajects = 17
-    r = RunExperiments(connections_file, locations_file, max_number_of_trajects, max_time,
-                        number_of_experiments1, number_of_experiments2 = None, algorithm1_type = algorithm1, algorithm2_type = algorithm2, use_randomise = False)
+    # algorithm1 = Greedy
+    # algorithm2 = None
+    # number_of_experiments1 = 1
+    # r = RunExperiments(
+    #     connections_file,
+    #     locations_file,
+    #     max_number_of_trajects,
+    #     max_time,
+    #     number_of_experiments1,
+    #     number_of_experiments2 = None,
+    #     algorithm1_type = algorithm1,
+    #     algorithm2_type = algorithm2,
+    #     start_trajects = start_trajects,
+    #     end_trajects = end_trajects,
+    #     lookahead_depth = None,
+    #     use_randomise = False
+    # )
     # r.run_first_algorithm()
+    # r.save_all_collected_data("Greedy")
+    # r.save_all_objects("Greedy", algorithm1)
+    # r.box_plot("Greedy")
+
+    #__________GreedyLookahead experiment: Heuristic of TrajectAnalyzer and Random____________________________
+    # Note: We use a lookahead_depth from [2:6]. This is because a lookahead_depth of 1 is the same as a
+    # greedy algoritm and a lookahead_depth of more than 6 will take too much memory to calculate so it will
+    # take too long.
+    # __________GreedyLookahead Heuristic of TrajectAnalyzer:
+    # algorithm1 = GreedyLookahead
+    # algorithm2 = None
+    # number_of_experiments1 = 2
+    # r = RunExperiments(
+    #     connections_file,
+    #     locations_file,
+    #     max_number_of_trajects,
+    #     max_time,
+    #     number_of_experiments1,
+    #     number_of_experiments2 = None,
+    #     algorithm1_type = algorithm1,
+    #     algorithm2_type = algorithm2,
+    #     start_trajects = start_trajects,
+    #     end_trajects = end_trajects,
+    #     lookahead_depth = 6
+    #     use_randomise = False
+    # )
+    # r.run_first_algorithm()
+    # r.save_all_collected_data("GreedyLookahead TrajectAnalyzer")
     # r.save_all_objects("GreedyLookahead TrajectAnalyzer", algorithm1)
     # r.box_plot("GreedyLookahead TrajectAnalyzer")
+
+    # __________GreedyLookahead Random:
+    # algorithm1 = GreedyLookahead
+    # algorithm2 = None
+    # number_of_experiments1 = 2
+    # r = RunExperiments(
+    #     connections_file,
+    #     locations_file,
+    #     max_number_of_trajects,
+    #     max_time,
+    #     number_of_experiments1,
+    #     number_of_experiments2 = None,
+    #     algorithm1_type = algorithm1,
+    #     algorithm2_type = algorithm2,
+    #     start_trajects = start_trajects,
+    #     end_trajects = end_trajects,
+    #     lookahead_depth = 6,
+    #     use_randomise = True
+    # )
+    # r.run_first_algorithm()
+    # r.save_all_objects("GreedyLookahead Random", algorithm1)
+    # r.save_all_collected_data("GreedyLookahead Random")
+    # r.box_plot("GreedyLookahead Random")
